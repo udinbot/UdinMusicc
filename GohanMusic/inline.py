0956e1c90874916227e995296aa36fdd82324c8b
nopalle <86257395+nopalle@users.noreply.github.com>
2021-08-14 11:08:43 +0700

    Update inline.py

--- GohanMusic/inline.py
+++ GohanMusic/inline.py
@@ -13,7 +13,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Ketikan nama Lagu/video di YouTube...",
+            switch_pm_text="ketik judul lagu/video di YouTube",
             switch_pm_parameter="help",
             cache_time=0
         )
